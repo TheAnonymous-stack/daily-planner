9f64e7c0641e3989744f508ccc60b506028a2784
TheAnonymous-stack <thanhnht.2014@gmail.com>
2022-10-21 00:01:28 -0700

    added the time picker

--- src/EventList.js
+++ src/EventList.js
@@ -1,11 +1,11 @@
-import { red } from '@mui/material/colors';
+
 import { Link } from 'react-router-dom';
 const EventList = ({ USER_EVENTS, title }) => {
     
     
 const eventStyle = {  
-    backgroundColor: "grey"
-    // display: "inline-block"
+    backgroundColor: "red",
+    display: "inline-block"
     
 
 };
--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+import TimePicker from "rc-time-picker";
+import 'rc-time-picker/assets/index.css';
+
 const Form = () => {
     const [eventType, setEventType] = useState('');
     const [time, setTime] = useState('');
@@ -36,37 +39,20 @@ const Form = () => {
                     value={eventType}
                     onChange={(e) => setEventType(e.target.value)}
                 />
-                <label>Time:</label>
-                <select
-                    value={time}
-                    onChange={(e) => setTime(e.target.value)}
-                >
-                    <option value="12AM">12AM</option>
-                    <option value="1AM">1AM</option>
-                    <option value="2AM">2AM</option>
-                    <option value="3AM">3AM</option>
-                    <option value="4AM">4AM</option>
-                    <option value="5AM">5AM</option>
-                    <option value="6AM">6AM</option>
-                    <option value="7AM">7AM</option>
-                    <option value="8AM">8AM</option>
-                    <option value="9AM">9AM</option>
-                    <option value="10AM">10AM</option>
-                    <option value="11AM">11AM</option>
-                    <option value="12PM">12PM</option>
-                    <option value="1PM">1PM</option>
-                    <option value="2PM">2PM</option>
-                    <option value="3PM">3PM</option>
-                    <option value="4PM">4PM</option>
-                    <option value="5PM">5PM</option>
-                    <option value="6PM">6PM</option>
-                    <option value="7PM">7PM</option>
-                    <option value="8PM">8PM</option>
-                    <option value="9PM">9PM</option>
-                    <option value="10PM">10PM</option>
-                    <option value="11PM">11PM</option>
+                {/* <label>Time:</label> */}
+                <div className="timePicker">
                     
-                </select>
+                    <br />
+                    <p>Selected Time: {time || ''}</p>
+                    <TimePicker
+                        placeholder="Select Time"
+                        use12Hours
+                        showSecond={false}
+                        focusOnOpen={true}
+                        format="hh:mm A"
+                        onChange={e => setTime(e.format('LT'))}
+                    />
+                </div>
                 { !isPending && <button>Add Event</button> }
                 { isPending && <button>Adding Event...</button> }
                 
